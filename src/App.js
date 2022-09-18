25691266544a58ff88c3776605910fa053d23e2a
ReggyCZ <113830993+ReggyCZ@users.noreply.github.com>
2022-09-18 13:56:03 +0200

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
+
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,62 +1,97 @@
-import { useState, useEffect } from "react";
-import { NavBar, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.png';
-import navIcon1 from '../assets/img/logo.png';
-import navIcon2 from '../assets/img/logo.png';
-import navIcon3 from '../assets/img/logo.png';
-import navIcon4 from '../assets/img/logo.png';
+import { useState, useEffect } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
-  const [scolled, seScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-useEffect(() => {
-  const onScroll = () => {
-    if (window.scrollY > 50) {
-      seScrolled(true);
-    } else {
-      seScrolled(false);
-    }
-  }
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-  window.addEventListener("scrolll", onscroll);
+    window.addEventListener('scroll', onScroll);
 
-  return () => window.removeEventListener("scroll", onScroll);
-}, [])
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
-const onUpdateActiveLink = (value) => {
-  setActiveLink(value);
-}
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
-  return {
-    <NavBar expand="lg" className={scrolled ? "scrolled": ""}>
-      <Container>
-        <NavBar.Brand href="#home">
-          <img src={logo} alt="Logo" />
-        </NavBar.Brand>
-        <NavBar.Toogle aria-controls="basic-navbar-nav"> 
-          <span className="navbar-toogle-icon"></span>
-        </NavBar.Toogle>
-        <NavBar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} OnClick={() => onUpdateActiveLink('home')}>Domů</Nav.Link>
-          <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} OnClick={() => onUpdateActiveLink('about')}>O mně</Nav.Link>
-          <Nav.Link href="#skill" className={activeLink === 'skill' ? 'active navbar-link' : 'navbar-link'} OnClick={() => onUpdateActiveLink('skills')}>Grafika</Nav.Link>
-          <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} OnClick={() => onUpdateActiveLink('contact')}>Kontakty</Nav.Link>
-          </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="" /></a>
-              <a href="#"><img src={navIcon2} alt="" /></a>
-              <a href="#"><img src={navIcon3} alt="" /></a>
-              <a href="#"><img src={navIcon4} alt="" /></a>
-            </div>
-            <button className="vvd" OnClick={() => console.log('connect')}><span>Připoj se</span></button>
-          </span>
-        </NavBar.Collapse>
-      </Container>
-    </NavBar>
-
-
-  }
-}
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? 'scrolled' : ''}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('home')}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('skills')}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === 'projects'
+                    ? 'active navbar-link'
+                    : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('projects')}
+              >
+                Projects
+              </Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="#">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="#">
+                  <img src={navIcon3} alt="" />
+                </a>
+              </div>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Let’s Connect</span>
+                </button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+};
